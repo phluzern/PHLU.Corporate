68ee4ba4d7413b2c88c29c739ce091ffb0c07be1
Egli Michael <pamegli@n0003574.local>
2016-06-14 16:48:04 +0200

    added animation to tag filter

--- Resources/Public/JavaScript/app.js
+++ Resources/Public/JavaScript/app.js
@@ -18,55 +18,70 @@
 
         $scope.toggleFilter = function(id) {
 
+            // reset filtered items
+            $scope.filteredIn = {};
+            $scope.filteredOut = {};
+
+
+            // unset filter all if other filters are set
             if (id !== 0) {
                 delete $scope.filter[0];
-
-
-                if ($scope.filter[id] === undefined) {
+                if ($scope.filter[id] === undefined || $scope.filter[id] === false) {
                     $scope.filter[id] = true;
                 } else {
                     delete $scope.filter[id];
                 }
             }
 
+            // toggle filter all
+            if (id === 0) {
+                $scope.filter[id] = $scope.filter[id] ? false : true;
 
+                angular.forEach($scope.filterAll, function (val,nodeId) {
+                    if (nodeId !== 0) {
+                        if (nodeId === id) {
+                            $scope.filter[nodeId] = true;
+                        } else {
+                            $scope.filter[nodeId] = $scope.filter[id] ? false : true;
+                        }
+                    }
+
+
+                });
+            }
+
+            // set filter all if no other filters are set
             if (Object.keys($scope.filter).length === 0) $scope.filter[0] = true;
 
-            $scope.filteredIn = {};
+
+            // apply filters to items list
             angular.forEach($scope.filterItems, function (tags, nodeId) {
 
                 if (id == 0) {
+                    // filter in all items
                     $scope.filteredIn[nodeId] = true;
                 } else {
-                    // filter items by tag
+                    // filter in items by tag
                     angular.forEach(tags, function (val, tag) {
-                        if ($scope.filter[tag] !== undefined) {
+                        if ($scope.filter[tag] !== undefined && $scope.filter[tag] !== false) {
                             $scope.filteredIn[nodeId] = true;
                         }
                     });
                 }
 
-
-                $scope.filteredOut = {};
                 if ($scope.filteredIn[nodeId] === undefined) {
                     $scope.filteredIn[nodeId] = false;
 
                     $timeout(function() {
                         $scope.filteredOut[nodeId] = true;
-                    }, 600);
+                    }, 500);
 
                 }
             });
 
-            if (id === 0) {
-                $scope.filter[id] = $scope.filter[id] ? false : true;
 
-                    angular.forEach($scope.filterAll, function (val,nodeId) {
-                        if (nodeId !== 0) {
-                            $scope.filter[nodeId] = $scope.filter[id] ? false : true;
-                        }
-                    });
-             }
+
+
 
         };
 
