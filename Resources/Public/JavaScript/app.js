6ac11b8f65660c1d10c3bfd3a6f371cbc0d33504
Egli Michael <pamegli@n0003574.local>
2016-06-14 15:27:17 +0200

    added tag filter app

--- Resources/Public/JavaScript/app.js
+++ Resources/Public/JavaScript/app.js
@@ -1,34 +1,72 @@
 'use strict';
 
-    var phluAppModule = angular.module('phluApp', []).config(function ($interpolateProvider) {
-        $interpolateProvider.startSymbol('}}}');
-        $interpolateProvider.endSymbol('}}}');
-    }).config(function ($anchorScrollProvider) {
-        $anchorScrollProvider.disableAutoScrolling();
-    })
+    var phluAppModule = angular.module('phluApp', []);
 
 
-    .controller('SubjectsFilterNavCtrl', ['$scope',function($scope) {
+
+    // PHLU.Corporate:Page.View.Default filter tag navigation
+    phluAppModule.controller('SubjectsFilterNavCtrl', ['$scope','$timeout',function($scope,$timeout) {
 
 
         $scope.filter = {};
         $scope.filter[0] = true;
+        $scope.filterAll = {};
         $scope.filteredIn = {};
+        $scope.filteredOut = {};
         $scope.filterItems = {};
+
 
         $scope.toggleFilter = function(id) {
 
-            if (id !== 0) delete $scope.filter[0];
+            if (id !== 0) {
+                delete $scope.filter[0];
 
-            if ($scope.filter[id] === undefined) {
-                $scope.filter[id] = true;
-            } else {
-                delete $scope.filter[id];
+
+                if ($scope.filter[id] === undefined) {
+                    $scope.filter[id] = true;
+                } else {
+                    delete $scope.filter[id];
+                }
             }
+
 
             if (Object.keys($scope.filter).length === 0) $scope.filter[0] = true;
 
-console.log(1);
+            $scope.filteredIn = {};
+            angular.forEach($scope.filterItems, function (tags, nodeId) {
+
+                if (id == 0) {
+                    $scope.filteredIn[nodeId] = true;
+                } else {
+                    // filter items by tag
+                    angular.forEach(tags, function (val, tag) {
+                        if ($scope.filter[tag] !== undefined) {
+                            $scope.filteredIn[nodeId] = true;
+                        }
+                    });
+                }
+
+
+                $scope.filteredOut = {};
+                if ($scope.filteredIn[nodeId] === undefined) {
+                    $scope.filteredIn[nodeId] = false;
+
+                    $timeout(function() {
+                        $scope.filteredOut[nodeId] = true;
+                    }, 600);
+
+                }
+            });
+
+            if (id === 0) {
+                $scope.filter[id] = $scope.filter[id] ? false : true;
+
+                    angular.forEach($scope.filterAll, function (val,nodeId) {
+                        if (nodeId !== 0) {
+                            $scope.filter[nodeId] = $scope.filter[id] ? false : true;
+                        }
+                    });
+             }
 
         };
 
@@ -37,22 +75,6 @@ console.log(1);
             $scope.filterItems[nodeId][filterId] = true;
         };
 
-        $scope.$watch('filter', function(filter) {
-            console.log($scope.filteredIn);
-            $scope.filteredIn = {};
-
-            angular.forEach($scope.filterItems, function (val, nodeId) {
-
-                if (filter[val] !== undefined) {
-                    $scope.filteredIn[nodeId] = true;
-                }
-
-            });
-
-
-
-
-        });
 
 
 
