350e74f5a90654895f966a4c95e0c76b6e0aca0f
Egli Michael <pamegli@n0003574.local>
2016-06-15 13:46:07 +0200

    refactored filter navigation

---
+++ Resources/Public/JavaScript/angularjs/PHLU/Corporate/Controller/SubjectsFilterNavCtrl.js
@@ -0,0 +1,111 @@
+
+
+// PHLU.Corporate:Page.View.Default filter tag navigation
+PHLUCorporateApp.controller('SubjectsFilterNavCtrl', ['$scope','$timeout','$cookies',function($scope,$timeout,$cookies) {
+
+
+
+    $scope.appId = null;
+    $scope.filter = {};
+    $scope.filter[0] = true;
+    $scope.filterAll = {};
+    $scope.filteredIn = {};
+    $scope.filteredOut = {};
+    $scope.filterItems = {};
+    $scope.animated = true;
+
+
+    $scope.toggleFilter = function(id) {
+
+
+        // unset filter all if other filters are set
+        if (id !== 0) {
+            delete $scope.filter[0];
+            if ($scope.filter[id] === undefined || $scope.filter[id] === false) {
+                $scope.filter[id] = true;
+            } else {
+                delete $scope.filter[id];
+            }
+        }
+
+        // toggle filter all
+        if (id === 0) {
+            $scope.filter[id] = $scope.filter[id] ? false : true;
+
+            angular.forEach($scope.filterAll, function (val,nodeId) {
+                if (nodeId !== 0) {
+                    if (nodeId === id) {
+                        $scope.filter[nodeId] = true;
+                    } else {
+                        $scope.filter[nodeId] = $scope.filter[id] ? false : true;
+                    }
+                }
+
+
+            });
+        }
+
+        // set filter all if no other filters are set
+        if (Object.keys($scope.filter).length === 0) $scope.filter[0] = true;
+
+        // apply filters
+        if (id == 0) {
+            // filter in all items
+            $scope.filteredIn = {};
+            $scope.filteredOut = {};
+        } else {
+            $scope.applyFilters(true);
+        }
+
+        $cookies.put($scope.appId, JSON.stringify({'filter': $scope.filter}));
+
+    };
+
+    $scope.applyFilters = function(animated) {
+
+        $scope.animated = animated === true ? true : false;
+
+        // apply filters to items list
+        angular.forEach($scope.filterItems, function (tags, nodeId) {
+
+            // filter in items by tag
+            var found = false;
+            angular.forEach(tags, function (val, tag) {
+                if ($scope.filter[tag] !== undefined && $scope.filter[tag] !== false) {
+                    found = true;
+                }
+            });
+
+
+            if (found === false) {
+                $scope.filteredIn[nodeId] = false;
+                $timeout(function () {
+                    $scope.filteredOut[nodeId] = true;
+                    $scope.filteredIn[nodeId] = false;
+                }, animated === false ? 0 : 500);
+            } else {
+                $scope.filteredOut[nodeId] = false;
+                $scope.filteredIn[nodeId] = true;
+            }
+
+        });
+    }
+
+
+    $scope.addFilterItem = function(nodeId,filterId) {
+        if ($scope.filterItems[nodeId] === undefined) $scope.filterItems[nodeId] = {};
+        $scope.filterItems[nodeId][filterId] = true;
+    };
+
+
+    // apply last states from cookies
+    $scope.$watch("appId", function(appId) {
+        $scope.filter = $cookies.get(appId) ? JSON.parse($cookies.get(appId)).filter : {};
+        if ($cookies.get(appId)) $scope.applyFilters(false);
+    })
+
+
+
+}]);
+
+
