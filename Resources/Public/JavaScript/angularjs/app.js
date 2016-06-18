fe6c339f8d62780dda3b3c823a35c9f51da2c86c
Egli Michael <pamegli@n0003574.local>
2016-06-18 18:10:03 +0200

    refactored

--- Resources/Public/JavaScript/angularjs/app.js
+++ Resources/Public/JavaScript/angularjs/app.js
@@ -2,3 +2,18 @@
 
 var PHLUCorporateApp = angular.module('PHLUCorporateApp', ['ngCookies']);
 
+
+
+if (typeof document.addEventListener === 'function') {
+    document.addEventListener('Neos.PageLoaded', function(event) {
+
+        // re-compile angular app
+        jQuery("*[data-ng-app]").each(function() {
+            try {
+                angular.bootstrap(jQuery(this), [jQuery(this).attr('data-ng-app')]);
+            } catch (e) {}
+
+        });
+
+    }, false);
+}
