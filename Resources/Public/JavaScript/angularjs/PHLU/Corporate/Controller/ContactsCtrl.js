ae0028cb252f9340e0cdbedd63f52c2d11e4305c
Egli Michael <pamegli@n0002945.campus.intern>
2016-06-23 18:14:45 +0200

    added contacts node type

---
+++ Resources/Public/JavaScript/angularjs/PHLU/Corporate/Controller/ContactsCtrl.js
@@ -0,0 +1,42 @@
+
+
+// PHLU.Corporate:Page.View.Default filter tag navigation
+PHLUCorporateApp.controller('ContactsCtrl', ['$scope','$timeout','$cookies',function($scope,$timeout,$cookies) {
+
+
+
+        $scope.detail = false;
+        $scope.promise = false;
+
+       $scope.hoverIn = function(e) {
+
+           if ($scope.promise) $timeout.cancel($scope.promise);
+
+           if ($(e.target).parent().attr('href') === undefined) {
+               $scope.promise = $timeout(function() {
+                   $scope.detail = true;
+               },500);
+           } else {
+
+           }
+
+
+
+
+       }
+
+       $scope.hoverOut = function() {
+           if ($scope.promise) $timeout.cancel($scope.promise);
+           $scope.detail = false;
+       }
+
+
+        $scope.init = function(e) {
+           $(e).removeClass('invisible');
+
+       }
+
+
+}]);
+
+
