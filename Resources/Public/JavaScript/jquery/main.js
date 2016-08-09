55b7ad1aec3e5c5263405632feca666fc3e5b40a
Adrian Felder <adrian.felder@phlu.ch>
2016-08-09 13:28:23 +0200

    removed max-width property for main contents in xxxl view and added js to have footer width in sync with main contents width / updated ie10 and 11 css files

--- Resources/Public/JavaScript/jquery/main.js
+++ Resources/Public/JavaScript/jquery/main.js
@@ -99,6 +99,16 @@ $(document).ready(function () {
     // }
 
     var resize = function () {
+        if ($(window).outerWidth() > 1600) {
+            var contentWidth = 0;
+            console.log($( ".level1" ).length);
+            if ( $( ".level1" ).length > 1) { contentWidth = 528 } else { contentWidth = 264 }
+            contentWidth += $('.main').outerWidth();
+            $(".footer").attr('style','width:'+ contentWidth + 'px');
+        }
+        else { $(".footer").removeAttr('style'); }
+
+
         if ($(window).outerWidth() > 1266) {
             /* set content height if (on startpage) the .aktuelles container is smaller than main container  */
             // if ($(".row.aktuelles").innerHeight() < $(".main").innerHeight()) {
@@ -107,7 +117,6 @@ $(document).ready(function () {
             /* add top margin for phlu bg pattern on level1 subnav on short pages */
             if ($(".sidebar").innerHeight() < 2000) {
                 $('.sidebar.level1').addClass('phlu-pattern-top-margin');
-
             }
         }
         else {
