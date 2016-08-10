7a366eac81e58c0de3a190b70c0b59580e4ae24f
miegli <michae.egli@phlu.ch>
2016-08-10 14:15:07 +0200

    added icon selector

---
+++ Classes/PHLU/Corporate/DataSource/TagsIconDataSource.php
@@ -0,0 +1,61 @@
+<?php
+namespace PHLU\Corporate\DataSource;
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Neos\Service\DataSource\AbstractDataSource;
+use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
+use PHLU\Neos\Models\Domain\Repository\ContactRepository;
+
+
+class TagsIconDataSource extends AbstractDataSource {
+
+
+    /**
+     * @var string
+     */
+    static protected $identifier = 'phlu-corporate-tags-icon';
+
+
+    /**
+     * @Flow\Inject
+     * @var ContactRepository
+     */
+    protected $contactRepository;
+
+    
+
+    /**
+     * Get data
+     *
+     * @param NodeInterface $node The node that is currently edited (optional)
+     * @param array $arguments Additional arguments (key / value)
+     * @return array JSON serializable data
+     */
+    public function getData(NodeInterface $node = NULL, array $arguments)
+    {
+
+
+        $icons = array();
+
+        // custom icons
+        $icons['faecherIco'] = array('value' => 'faecherIco', 'label' => 'Fächer', 'icon' => '');
+        $icons['strategieIco'] = array('value' => 'strategieIco', 'label' => 'Strategische Schwerpunkte', 'icon' => '');
+        $icons['forschungIco'] = array('value' => 'forschungIco', 'label' => 'Forschungschwerpunkte', 'icon' => '');
+        $icons['themaIco'] = array('value' => 'themaIco', 'label' => 'Thematische Schwerpunkte', 'icon' => '');
+
+        // iconic icons
+        //$icons['ico-family-iconic oi-briefcase'] = array('value' => 'ico-family-iconic oi-briefcase', 'label' => 'Brieftasche', 'icon' => 'ico-family-iconic oi-briefcase');
+
+        // fontawesome icons
+        //$icons['ico-family-fa fa-tag'] = array('value' => 'ico-family-fa fa-tag', 'label' => 'Tag', 'icon' => 'ico-family-fa fa-tag');
+
+
+        return $icons;
+
+
+    }
+
+
+
+
+}
