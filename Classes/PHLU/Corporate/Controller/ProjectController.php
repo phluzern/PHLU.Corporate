693268b53ef917815e1290397bd6d6352b820a36
miegli <michae.egli@phlu.ch>
2016-08-23 18:24:24 +0200

    added ppdb project templates and more sync functions added collection support for imported contact images

--- Classes/PHLU/Corporate/Controller/ProjectController.php
+++ Classes/PHLU/Corporate/Controller/ProjectController.php
@@ -5,8 +5,8 @@ namespace PHLU\Corporate\Controller;
  * This file is part of the PHLU.Corporate package.
  */
 
+use PHLU\Neos\Models\Domain\Model\Project;
 use TYPO3\Flow\Annotations as Flow;
-use TYPO3\Flow\Http\Request;
 use TYPO3\Neos\Domain\Repository\SiteRepository;
 use TYPO3\Neos\Domain\Service\ContentContextFactory;
 use TYPO3\TYPO3CR\Domain\Model\Node;
@@ -18,7 +18,7 @@ use TYPO3\TYPO3CR\Exception\PageNotFoundException;
 use TYPO3\Neos\Domain\Model\Site;
 
 
-class PortraitController extends \TYPO3\Neos\Controller\Frontend\NodeController
+class ProjectController extends \TYPO3\Neos\Controller\Frontend\NodeController
 {
 
     /**
@@ -52,62 +52,32 @@ class PortraitController extends \TYPO3\Neos\Controller\Frontend\NodeController
 
 
     /**
-     * Shows the specified node and takes visibility and access restrictions into
-     * account.
+     * Shows the specified node and view project details
      *
-     * @param NodeInterface $node
-     * @param Contact $contact person identifier
+     * @param integer $project project
      * @return string View output for the specified node
      * @Flow\SkipCsrfProtection We need to skip CSRF protection here because this action could be called with unsafe requests from widgets or plugins that are rendered on the node - For those the CSRF token is validated on the sub-request, so it is safe to be skipped here
      * @Flow\IgnoreValidation("node")
      * @throws NodeNotFoundException
      */
-    public function viewAction($node, $contact)
+    public function loadAction($project)
     {
-
-
-        if ($node === null) {
-            throw new NodeNotFoundException('The requested node does not exist or isn\'t accessible to the current user', 1430218623);
-        }
-        if (!$node->getContext()->isLive() && !$this->privilegeManager->isPrivilegeTargetGranted('TYPO3.Neos:Backend.GeneralAccess')) {
-            $this->redirect('index', 'Login', null, array('unauthorized' => true));
-        }
-
-
-
-        // show only pages with containing contact nodes inside
-        //        $flowQuery = new FlowQuery(array($node));
-        //        if ($flowQuery->find("[instanceof PHLU.Corporate:Contact][contact=" . $contact->getEventoid() . "]")->count() == 0) {
-        //            throw new PageNotFoundException('The requested node does not exist or isn\'t accessible to the current user', 1430218623);
-        //        }
-
-
-
-        $this->view->assignMultiple(array('value' => $node, 'contact' => $contact));
-
-        $this->view->setTypoScriptPath('portrait');
 
 
     }
 
     /**
-     * Shows the specified node and takes visibility and access restrictions into
-     * account.
+     * Shows the specified node and view project details
      *
-     * @param Contact $contact person identifier
+     * @param Project $project project
      * @return string View output for the specified node
      * @Flow\SkipCsrfProtection We need to skip CSRF protection here because this action could be called with unsafe requests from widgets or plugins that are rendered on the node - For those the CSRF token is validated on the sub-request, so it is safe to be skipped here
      * @Flow\IgnoreValidation("node")
      * @throws NodeNotFoundException
      */
-    public function viewPortraitAction($contact)
+    public function viewAction($project)
     {
 
-
-        // redirect non .html uri
-        if (substr($this->controllerContext->getRequest()->getParentRequest()->getUri()->getPath(),-5,5) !== '.html') {
-           $this->controllerContext->getResponse()->setHeader('Location', $this->controllerContext->getRequest()->getParentRequest()->getUri()->getPath().".html");
-        }
 
         $currentSite = $this->siteRepository->findOneByNodeName('corporate');
 
@@ -119,11 +89,14 @@ class PortraitController extends \TYPO3\Neos\Controller\Frontend\NodeController
         /** @var ContentContext $contentContext */
         $contentContext = $this->createContext($currentSite, 'live', array());
         $node = new Node(
-            $this->nodeDataRepository->findOneByIdentifier('c8192de7-7700-4c43-aff3-6fb5214efa54', $this->workspaceRepository->findByName('live')->getFirst()),
+            $this->nodeDataRepository->findOneByIdentifier('fc3e89af-ca93-4f2d-ab77-a2bd698f291f', $this->workspaceRepository->findByName('live')->getFirst()),
             $contentContext);
 
-        $this->view->assignMultiple(array('value' => $node, 'contact' => $contact));
-        $this->view->setTypoScriptPath('portrait');
+
+
+
+        $this->view->assignMultiple(array('value' => $node, 'project' => $project));
+        $this->view->setTypoScriptPath('project');
 
 
     }
