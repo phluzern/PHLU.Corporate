149bcc973187e26a0532b80ef2f553651d091b25
miegli <michae.egli@phlu.ch>
2016-10-28 15:39:48 +0200

    added more hybridsearch features

--- Resources/Public/JavaScript/angularjs/PHLU/Corporate/Controller/PpdbCtrl.js
+++ Resources/Public/JavaScript/angularjs/PHLU/Corporate/Controller/PpdbCtrl.js
@@ -1,12 +1,22 @@
 PHLUCorporateApp.controller('PpdbCtrl', ['$scope', 'hybridsearch', '$hybridsearchObject', '$hybridsearchResultsObject', function ($scope, hybridsearch, $hybridsearchObject, $hybridsearchResultsObject) {
 
     var search = new $hybridsearchObject(hybridsearch);
+    var search2 = new $hybridsearchObject(hybridsearch);
     //$scope.result = new $hybridsearchResultsObject();
     $scope.lifetime = {};
 
     // .addNodesByIdentifier(['fsdfsdf-fsdffsdf'.'fsdfsdfdf-fsdfsdfsdf'])
 
-    search.setQuery("search", $scope).addPropertyFilter('title', '', null, true).addPropertyFilter('lifetime', 'lifetime', $scope).setNodeType('phlu-neos-nodetypes-project').$bind('result',$scope);
+    //search.setQuery("search", $scope).addPropertyFilter('title', '', null, true).addPropertyFilter('lifetime', 'lifetime', $scope).setNodeType('phlu-neos-nodetypes-project').$bind('result',$scope);
+
+
+    search.setQuery("search", $scope)
+        .setQuery("search", $scope)
+        .addPropertyFilter('title', '', null, true)
+        .addPropertyFilter('lifetime', 'lifetime', $scope)
+        .addNodesByNodeTypes(['phlu-neos-nodetypes-project'])
+        .$bind('result', $scope);
+
 
 }]);
 
