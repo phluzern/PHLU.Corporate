bff0e7cab3774bb849f3b2884fccfbbd8acceaa3
miegli <michae.egli@phlu.ch>
2016-11-03 14:07:39 +0100

    added multi arrays filter support

--- Resources/Public/JavaScript/angularjs/PHLU/Corporate/Controller/PpdbCtrl.js
+++ Resources/Public/JavaScript/angularjs/PHLU/Corporate/Controller/PpdbCtrl.js
@@ -1,21 +1,38 @@
 PHLUCorporateApp.controller('PpdbCtrl', ['$scope', 'hybridsearch', '$hybridsearchObject', '$hybridsearchResultsObject', function ($scope, hybridsearch, $hybridsearchObject, $hybridsearchResultsObject) {
 
-    var search = new $hybridsearchObject(hybridsearch);
-    var search2 = new $hybridsearchObject(hybridsearch);
+    $scope.list = new $hybridsearchObject(hybridsearch);
+    $scope.result = new $hybridsearchResultsObject();
+    $scope.search = '';
+    $scope.filterLifetime = [];
+
+    $scope.list
+        .addPropertyFilter('lifetime', 'filterLifetime', $scope)
+        .setNodeType('phlu-neos-nodetypes-project')
+        .addPropertyFilter('title', '', null, true)
+        .$bind('result',$scope)
+
+
+    $scope.$watch('filterLifetime',function(i) {
+        console.log($scope.list.$$app.getFilter());
+
+    });
+
+
+    //var search2 = new $hybridsearchObject(hybridsearch);
     //$scope.result = new $hybridsearchResultsObject();
-    $scope.lifetime = {};
+    //$scope.lifetime = {};
 
     // .addNodesByIdentifier(['fsdfsdf-fsdffsdf'.'fsdfsdfdf-fsdfsdfsdf'])
-
     //search.setQuery("search", $scope).addPropertyFilter('title', '', null, true).addPropertyFilter('lifetime', 'lifetime', $scope).setNodeType('phlu-neos-nodetypes-project').$bind('result',$scope);
 
 
-    search.setQuery("search", $scope)
-        .setQuery("search", $scope)
-        .addPropertyFilter('title', '', null, true)
-        .addPropertyFilter('lifetime', 'lifetime', $scope)
-        .setNodeType('phlu-neos-nodetypes-project')
-        .$bind('result', $scope);
+    // search.setQuery("search", $scope)
+    //     .setQuery("search", $scope)
+    //     .addPropertyFilter('title', '', null, true)
+    //     .addPropertyFilter('lifetime', 'lifetime', $scope)
+    //     .setNodeType('phlu-neos-nodetypes-project')
+    //     .$bind('result', $scope);
+
 
 
 }]);
