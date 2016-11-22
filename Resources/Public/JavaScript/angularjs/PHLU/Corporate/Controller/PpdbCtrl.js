ffa9dcc83f0e662e3468cbcd6e0ea63e338713a1
miegli <michae.egli@phlu.ch>
2016-11-22 14:07:39 +0100

    added person import support for ppdb publications

--- Resources/Public/JavaScript/angularjs/PHLU/Corporate/Controller/PpdbCtrl.js
+++ Resources/Public/JavaScript/angularjs/PHLU/Corporate/Controller/PpdbCtrl.js
@@ -19,40 +19,29 @@ PHLUCorporateApp.controller('PpdbCtrl', ['$scope', 'hybridsearch', '$hybridsearc
     }
 
     $scope.setFilterLifetime = function (filterLifetime) {
-        angular.forEach(filterLifetime, function (filter) {
-            $scope.filterLifetime[filter] = true;
-        });
+        $scope.filterLifetime = f;
         $scope.initialFilters['lifetime'] = true;
     };
 
     $scope.setFilterResearchunit = function (f) {
-        angular.forEach(f, function (filter) {
-            $scope.researchunit[filter] = true;
-        });
+        $scope.researchunit = f;
         $scope.initialFilters['researchunit'] = true;
     };
 
     $scope.setFilterFinancingtypes = function (f) {
-        angular.forEach(f, function (filter) {
-            $scope.financingtype[filter] = true;
-        });
+        $scope.financingtype = f;
         $scope.initialFilters['financingtype'] = true;
     };
 
     $scope.setFilterProjectparticipants = function (f) {
-        angular.forEach(f, function (filter) {
-            $scope.projectparticipants[filter] = true;
-        });
+        $scope.projectparticipants = f;
         $scope.initialFilters['participants'] = true;
     };
 
     $scope.setFilterResearchmainfocus = function (f) {
-        angular.forEach(f, function (filter) {
-            $scope.researchmainfocus[filter] = true;
-        });
         $scope.initialFilters['researchmainfocus'] = true;
+        $scope.researchmainfocus = f;
     };
-
 
 
     $scope.list
@@ -66,35 +55,32 @@ PHLUCorporateApp.controller('PpdbCtrl', ['$scope', 'hybridsearch', '$hybridsearc
         .$bind('result', $scope);
 
 
-
 }]);
 
 PHLUCorporateApp.controller('PpdbPublicationCtrl', ['$scope', 'hybridsearch', '$hybridsearchObject', '$hybridsearchResultsObject', function ($scope, hybridsearch, $hybridsearchObject, $hybridsearchResultsObject) {
 
     $scope.list = new $hybridsearchObject(hybridsearch);
     $scope.result = new $hybridsearchResultsObject();
-    $scope.search = '';
+    $scope.participants = {};
+    $scope.initialFilters = {};
 
 
-    $scope.addPropertyFilter = function (property, value) {
-        $scope.list.addPropertyFilter(property, value, $scope);
-    }
-
-    $scope.setQuery = function (value) {
-        $scope.list.setQuery(value, $scope);
-    }
-
+    $scope.setFilterParticipants = function (f) {
+        $scope.participants = f;
+        $scope.initialFilters['participants'] = true;
+    };
 
     $scope.list
         .setNodeType('phlu-neos-nodetypes-publication')
         .addPropertyFilter('title', '', null, true)
+        .addPropertyFilter('persons.EventoID', 'participants', $scope)
         .$bind('result', $scope);
 
 }]);
 
 
-PHLUCorporateApp.filter('projectparticipantsFilter', function() {
-    return function(input, term) {
+PHLUCorporateApp.filter('projectparticipantsFilter', function () {
+    return function (input, term) {
 
         if (term === undefined || term == '') {
             return input;
@@ -105,14 +91,11 @@ PHLUCorporateApp.filter('projectparticipantsFilter', function() {
         angular.forEach(input, function (val, key) {
 
 
-
             if (JSON.stringify(val.value).indexOf(term) >= 0) {
                 collection[key] = val;
             }
 
         });
-
-
 
 
         return collection;
