34217f10cd87812a976065d91e3d8191ee2545c7
miegli <michae.egli@phlu.ch>
2016-11-29 14:30:35 +0100

    added publication filters

--- Resources/Public/JavaScript/angularjs/PHLU/Corporate/Controller/PpdbCtrl.js
+++ Resources/Public/JavaScript/angularjs/PHLU/Corporate/Controller/PpdbCtrl.js
@@ -71,7 +71,11 @@ PHLUCorporateApp.controller('PpdbPublicationCtrl', ['$scope', 'hybridsearch', '$
     $scope.participants = {};
     $scope.organisations = {};
     $scope.initialFilters = {};
+    $scope.participantsSearch = '';
 
+    $scope.setQuery = function (value) {
+        $scope.list.setQuery(value, $scope);
+    }
 
     $scope.setFilterParticipants = function (f) {
         $scope.participants = f;
@@ -93,7 +97,25 @@ PHLUCorporateApp.controller('PpdbPublicationCtrl', ['$scope', 'hybridsearch', '$
 }]);
 
 
-PHLUCorporateApp.filter('projectparticipantsFilter', function () {
+PHLUCorporateApp.filter('toArray', function () {
+    return function (obj, addKey) {
+        if (!angular.isObject(obj)) return obj;
+        if ( addKey === false ) {
+            return Object.keys(obj).map(function(key) {
+                return obj[key];
+            });
+        } else {
+            return Object.keys(obj).map(function (key) {
+                var value = obj[key];
+                return angular.isObject(value) ?
+                    Object.defineProperty(value, '$key', { enumerable: false, value: key}) :
+                { $key: key, $value: value };
+            });
+        }
+    };
+});
+
+PHLUCorporateApp.filter('inArrayFilter', function () {
     return function (input, term) {
 
         if (term === undefined || term == '') {
@@ -115,4 +137,5 @@ PHLUCorporateApp.filter('projectparticipantsFilter', function () {
         return collection;
 
     };
-})
+});
+
