8ed06a2bddc6fd5a0bc99348645c541626ecdf58
miegli <michae.egli@phlu.ch>
2017-01-13 18:52:08 +0100

    added widget prototype for further education special search

--- Resources/Public/JavaScript/angularjs/PHLU/Corporate/Controller/EventoCtrl.js
+++ Resources/Public/JavaScript/angularjs/PHLU/Corporate/Controller/EventoCtrl.js
@@ -41,3 +41,74 @@ PHLUCorporateApp.controller('EventoCtrl', ['$scope', 'hybridsearch', '$hybridsea
 
 }]);
 
+PHLUCorporateApp.controller('EventoFurtherEducationCtrl', ['$scope', 'hybridsearch', '$hybridsearchObject', '$hybridsearchResultsObject', function ($scope, hybridsearch, $hybridsearchObject, $hybridsearchResultsObject) {
+
+    var search = new $hybridsearchObject(hybridsearch);
+    $scope.result = new $hybridsearchResultsObject();
+    $scope.limit = 15;
+    $scope.limitChunkSize = 15;
+    $scope.graduation = {};
+    $scope.nodetypes = [];
+    $scope.nodetypesFilter = [
+
+        {state:false, id: 'all', label: 'Alle'},
+        {state:false, id: 'phlu-neos-nodetypes-course-study-furthereducation', label: 'Studiengang'},
+        {state:false, id: 'phlu-neos-nodetypes-course-module-furthereducation', label: 'Modul'},
+
+    ];
+    $scope.nodetypesFilterCurrentLabel = 'Alle';
+
+    $scope.setNodetypesFilter = function(filter) {
+
+        angular.forEach($scope.nodetypesFilter, function (val,key) {
+            val.state = false;
+        });
+        filter.state = true;
+        $scope.nodetypesFilterCurrentLabel = filter.label;
+
+        var nodetype = [];
+            angular.forEach($scope.nodetypesFilter, function (val,key) {
+                if (val.state || filter.id == 'all') {
+                    if (val.id !== 'all') {
+                        nodetype.push(val.id);
+                    }
+                }
+
+            });
+
+        $scope.nodetypes = nodetype;
+
+    }
+
+    $scope.setNodetypesFilter($scope.nodetypesFilter[0]);
+
+    $scope.loadMore = function () {
+        $scope.limit = $scope.limit + $scope.limitChunkSize;
+    };
+
+
+    $scope.setFilterNodeTypes = function (f) {
+       // $scope.initialFilters['nodetypes']
+    };
+
+    $scope.sizeOf = function (obj) {
+        if (obj === undefined) {
+            return 0;
+        }
+        return Object.keys(obj).length;
+    };
+
+    $scope.clearFilter = function(filtertype) {
+        $scope[filtertype] = {};
+    };
+
+    search
+        .setQuery('search',$scope)
+        .setOrderBy({'phlu-neos-nodetypes-course-study-furthereducation': 'nr'})
+        .addPropertyFilter('graduation', 'graduation', $scope)
+        .setNodeType('nodetypes',$scope)
+        .$bind('result', $scope)
+        .run();
+
+}]);
+
