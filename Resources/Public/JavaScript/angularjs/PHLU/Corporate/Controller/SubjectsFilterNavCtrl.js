40dfb513140729ca06bfdd5a6f2d9893712b14a8
miegli <michae.egli@phlu.ch>
2017-01-20 13:08:01 +0100

    added filter search

--- Resources/Public/JavaScript/angularjs/PHLU/Corporate/Controller/SubjectsFilterNavCtrl.js
+++ Resources/Public/JavaScript/angularjs/PHLU/Corporate/Controller/SubjectsFilterNavCtrl.js
@@ -1,10 +1,11 @@
-
-
 // PHLU.Corporate:Page.View.Default filter tag navigation
-PHLUCorporateApp.controller('SubjectsFilterNavCtrl', ['$scope','$timeout','$cookies',function($scope,$timeout,$cookies) {
+PHLUCorporateApp.controller('SubjectsFilterNavCtrl', ['$scope', 'hybridsearch', '$hybridsearchObject', '$hybridsearchResultsObject', '$timeout', '$cookies', function ($scope, hybridsearch, $hybridsearchObject, $hybridsearchResultsObject, $timeout, $cookies) {
 
 
-
+    $scope.list = new $hybridsearchObject(hybridsearch);
+    $scope.result = new $hybridsearchResultsObject();
+    $scope.search = '';
+    $scope.distinctUri = [];
     $scope.appId = null;
     $scope.filter = {};
     $scope.filter[0] = true;
@@ -12,10 +13,57 @@ PHLUCorporateApp.controller('SubjectsFilterNavCtrl', ['$scope','$timeout','$cook
     $scope.filteredIn = {};
     $scope.filteredOut = {};
     $scope.filterItems = {};
+    $scope.filterItemsWithPath = {};
+
     $scope.animated = true;
 
 
-    $scope.toggleFilter = function(id) {
+    $scope.list
+        .$bind('result', $scope)
+        .setNodeType(['phlu-corporate-text', 'phlu-corporate-headline','phlu-corporate-contact'])
+        .setNodePath('/faecher-und-schwerpunkte')
+        .setNodeTypeLabels({'*': 'Seiten'})
+        .setGroupedBy({'Seiten': 'url'})
+        .setQuery('search', $scope)
+        .$watch(function (data) {
+
+            var distinctUri = [];
+            angular.forEach(data.getNodes(), function (node) {
+                if (node.getScore() > 0.9) {
+                    distinctUri.push(node.uri.path);
+                }
+            });
+
+
+            angular.forEach($scope.filterItemsWithPath, function (path,item) {
+
+                if ($scope.search !== '' && distinctUri.indexOf(path) == -1) {
+                    $scope.filteredIn[item] = false;
+                    $scope.filteredOut[item] = true;
+                } else {
+                    $scope.filteredOut[item] = false;
+                    $scope.filteredIn[item] = true;
+                }
+
+            });
+
+
+
+            setTimeout(function () {
+                $scope.$apply(function () {
+
+
+
+                    $scope.distinctUri = distinctUri;
+                  //  console.log(distinctUri);
+                });
+            }, 10);
+
+        });
+
+
+
+    $scope.toggleFilter = function (id) {
 
 
         // unset filter all if other filters are set
@@ -32,7 +80,7 @@ PHLUCorporateApp.controller('SubjectsFilterNavCtrl', ['$scope','$timeout','$cook
         if (id === 0) {
             $scope.filter[id] = $scope.filter[id] ? false : true;
 
-            angular.forEach($scope.filterAll, function (val,nodeId) {
+            angular.forEach($scope.filterAll, function (val, nodeId) {
                 if (nodeId !== 0) {
                     if (nodeId === id) {
                         $scope.filter[nodeId] = true;
@@ -61,7 +109,7 @@ PHLUCorporateApp.controller('SubjectsFilterNavCtrl', ['$scope','$timeout','$cook
 
     };
 
-    $scope.applyFilters = function(animated) {
+    $scope.applyFilters = function (animated) {
 
         $scope.animated = animated === true ? true : false;
 
@@ -92,21 +140,33 @@ PHLUCorporateApp.controller('SubjectsFilterNavCtrl', ['$scope','$timeout','$cook
     }
 
 
-    $scope.addFilterItem = function(nodeId,filterId) {
+    $scope.addFilterItem = function (nodeId, filterId,path) {
+
         if ($scope.filterItems[nodeId] === undefined) $scope.filterItems[nodeId] = {};
         $scope.filterItems[nodeId][filterId] = true;
+
+        if ($scope.filterItemsWithPath[nodeId] === undefined) {
+            $scope.filterItemsWithPath[nodeId] = {};
+        }
+        if (path !== undefined) {
+            $scope.filterItemsWithPath[nodeId] = path;
+        }
+
+
     };
 
 
     // apply last states from cookies
-    $scope.$watch("appId", function(appId) {
-        if ($("#"+appId).find(".ng-tag").length === 0) $cookies.remove(appId);
+    $scope.$watch("appId", function (appId) {
+        if ($("#" + appId).find(".ng-tag").length === 0) $cookies.remove(appId);
         $scope.filter = $cookies.get(appId) ? JSON.parse($cookies.get(appId)).filter : {};
         if ($cookies.get(appId)) $scope.applyFilters(false);
     })
 
 
-
 }]);
+
+
+
 
 
