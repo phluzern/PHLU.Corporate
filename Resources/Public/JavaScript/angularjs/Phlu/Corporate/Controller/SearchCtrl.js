7fcb31b80992f28a3b111e0de5ca712715111aaa
miegli <michae.egli@phlu.ch>
2017-03-10 16:25:30 +0100

    added label folders

--- Resources/Public/JavaScript/angularjs/Phlu/Corporate/Controller/SearchCtrl.js
+++ Resources/Public/JavaScript/angularjs/Phlu/Corporate/Controller/SearchCtrl.js
@@ -357,22 +357,44 @@ PhluCorporateApp.controller('SearchCtrl', ['$scope', '$rootScope', '$sce', 'hybr
                 }
             }
 
+            // ,
+            // {
+            //
+            //     http: "/swissbib/$query",
+            //     parser: {
+            //         type: 'xml',
+            //         config: {
+            //             'nodeType': 'bibliothek',
+            //             'results': {'selector': 'rss.channel.item'},
+            //             'fields': {
+            //                 'bibliothek-title': 'title',
+            //                 'bibliothek-format': 'format',
+            //                 'bibliothek-creator': 'creator',
+            //                 'url': 'link',
+            //                 'bibliothek-pubDate': 'pubDate',
+            //                 'bibliothek-author': 'author'
+            //             }
+            //         }
+            //     }
+            // }
+
             ,
             {
 
-                http: "/swissbib/$query",
+                http: "/iluplus/$query",
                 parser: {
-                    type: 'xml',
+                    type: 'json',
                     config: {
                         'nodeType': 'bibliothek',
-                        'results': {'selector': 'rss.channel.item'},
                         'fields': {
-                            'bibliothek-title': 'title',
-                            'bibliothek-format': 'format',
-                            'bibliothek-creator': 'creator',
+                            'bibliothek-title': 'EXLResultTitle',
+                            'bibliothek-format': 'EXLThumbnailCaption',
+                            'bibliothek-autor': 'EXLResultAuthor',
+                            'bibliothek-isbn': 'EXLResultFourthLine',
+                            'bibliothek-details': 'EXLResultDetails',
+                            'rawcontent': 'rawcontent',
                             'url': 'link',
-                            'bibliothek-pubDate': 'pubDate',
-                            'bibliothek-author': 'author'
+                            'image': 'image',
                         }
                     }
                 }
