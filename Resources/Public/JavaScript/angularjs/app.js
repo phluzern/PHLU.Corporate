035d2526294d3b0d452724ca36f853d72823b273
miegli <michae.egli@phlu.ch>
2017-04-07 12:03:35 +0200

    changed cdn

--- Resources/Public/JavaScript/angularjs/app.js
+++ Resources/Public/JavaScript/angularjs/app.js
@@ -76,7 +76,7 @@ PhluCorporateApp.controller('initController', ['$scope','$hybridsearch', functio
           PhluCorporateApp.$$conf.workspaceName = workspaceName !== '' ? workspaceName : 'live';
           PhluCorporateApp.$$conf.siteNodeName = siteNodeName !== '' ? siteNodeName : 'corporate';
           PhluCorporateApp.$$conf.dimensionHash = dimensionHash !== '' ? dimensionHash : 'fb11fdde869d0a8fcfe00a2fd35c031d';
-          PhluCorporateApp.$$conf.cdnHost = window.location.host == 'phlu.ch.phlu-eduweb5.nine.ch' ? 'https://d1zsa2wyk011a7.cloudfront.net' : undefined;
+          PhluCorporateApp.$$conf.cdnHost = window.location.host == 'phlu.ch.phlu-eduweb5.nine.ch' ? 'https://d2or9b3ovniy97.cloudfront.net' : undefined;
   };
 
 
