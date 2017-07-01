43c8cb836bb119a7a57e71669f2ab8117bcdb723
miegli <michae.egli@phlu.ch>
2017-07-01 21:47:58 +0200

    added algolia integration

--- Resources/Public/JavaScript/angularjs/Phlu/Corporate/Controller/SearchCtrl.js
+++ Resources/Public/JavaScript/angularjs/Phlu/Corporate/Controller/SearchCtrl.js
@@ -749,7 +749,8 @@ PhluCorporateApp.controller('SearchCtrl', ['$scope', '$rootScope', '$sce', 'hybr
     var lasthash = null;
 
     //search.disableRealtime();
-    search.enableCache();
+    //search.enableCache();
+    search.enableAlgolia('BPO2FNRO2H','989d3e117604044fb26d2f4b63352087')
     search.setEmojis(emojs);
     search.addPropertyFilter('lastname', '', null, true, false, 'phlu-corporate-contact');
     search.addPropertyFilter('phone', '', null, true, false, 'phlu-corporate-contact');
