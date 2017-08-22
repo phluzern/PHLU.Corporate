494f99d88c016a9f16709cbe8d86a9d2cb335778
miegli <michae.egli@phlu.ch>
2017-08-22 14:34:16 +0200

    added cdn host

--- Resources/Public/JavaScript/angularjs/app.js
+++ Resources/Public/JavaScript/angularjs/app.js
@@ -60,8 +60,8 @@ PhluCorporateApp.factory('hybridsearch', ['$hybridsearch', function ($hybridsear
         PhluCorporateApp.$$conf.workspaceName,
         PhluCorporateApp.$$conf.dimensionHash,
         PhluCorporateApp.$$conf.siteNodeName,
-        PhluCorporateApp.$$conf.cdnHost
-        //PhluCorporateApp.$$conf.cdnStaticHost
+        PhluCorporateApp.$$conf.cdnHost,
+        PhluCorporateApp.$$conf.cdnStaticHost
     );
 
 
@@ -76,8 +76,8 @@ PhluCorporateApp.controller('initController', ['$scope', '$hybridsearch', functi
         PhluCorporateApp.$$conf.workspaceName = workspaceName !== '' ? workspaceName : 'live';
         PhluCorporateApp.$$conf.siteNodeName = siteNodeName !== '' ? siteNodeName : 'corporate';
         PhluCorporateApp.$$conf.dimensionHash = dimensionHash !== '' ? dimensionHash : 'fb11fdde869d0a8fcfe00a2fd35c031d';
-        //PhluCorporateApp.$$conf.cdnHost = window.location.host == 'phlu.ch.phlu-eduweb5.nine.ch' || window.location.host == 'phlu.ch'  || window.location.host == 'www.phlu.ch' ? 'https://dhummbq34u1xf.cloudfront.net' : undefined;
-        //PhluCorporateApp.$$conf.cdnStaticHost = window.location.host == 'phlu.ch.phlu-eduweb5.nine.ch' || window.location.host == 'phlu.ch'  || window.location.host == 'www.phlu.ch' ? 'https://d912ndzw4fx9a.cloudfront.net' : undefined;
+        PhluCorporateApp.$$conf.cdnHost = window.location.host == window.location.host == 'www.phlu.ch'  ? 'https://d1ablfrhie1735.cloudfront.net' : undefined;
+        PhluCorporateApp.$$conf.cdnStaticHost = window.location.host == 'www.phlu.ch' ? 'https://d3iynkir7cmkd3.cloudfront.net' : undefined;
     };
 
 
