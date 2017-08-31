57f18a9a65ade4a6b5b425fcf9a3bbabb3e63adc
miegli <michae.egli@phlu.ch>
2017-08-31 09:49:14 +0200

    added no cache parameter to .js files

---
+++ Classes/Phlu/Corporate/ViewHelpers/VersionControl/GetCurrentVersionViewHelper.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Phlu\Corporate\ViewHelpers\VersionControl;
+
+/*
+ * This file is part of the Neos.Neos package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\Flow\Annotations as Flow;
+use Neos\FluidAdaptor\Core\ViewHelper\AbstractViewHelper;
+/**
+ * Location view helper
+ */
+class GetCurrentVersionViewHelper extends AbstractViewHelper
+{
+
+    /**
+     * Disable escaping of tag based ViewHelpers so that the rendered tag is not htmlspecialchar'd
+     *
+     * @var boolean
+     */
+    protected $escapeOutput = FALSE;
+
+
+
+    /**
+     * Get version hash of current git commit
+     *
+     * @return string The current version/commit hast of git
+     * @throws ViewHelperException
+     */
+    public function render()
+    {
+        return file_get_contents('resource://Phlu.Corporate/Private/version.txt');
+    }
+
+
+
+}
